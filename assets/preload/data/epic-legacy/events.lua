7a6dd7b6f9e85c450f2fff18ed34408cbabeb2cd
Poyo2007 <itssng7@gmail.com>
2022-08-16 05:03:32 -0400

    reset lol

---
+++ assets/preload/data/epic-legacy/events.lua
@@ -0,0 +1,9 @@
+function onBeatHit()
+  if curBeat == 200 then
+    doTweenAlpha('alphaBG', 'bg', 0.3, 0.5, 'linear')
+  end
+  
+  if curBeat == 304 then
+    doTweenAlpha('alphaBG', 'bg', 0.3, 0.5, 'linear')
+  end
+end
