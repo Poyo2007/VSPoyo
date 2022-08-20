09207514d1520509533c8f4bf4fc1af094a1b88e
Poyo2007 <itssng7@gmail.com>
2022-08-20 07:49:18 -0400

    lets remove flashing state

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -201,11 +201,6 @@ class TitleState extends MusicBeatState
 		#elseif CHARTING
 		MusicBeatState.switchState(new ChartingState());
 		#else
-		if(FlxG.save.data.flashing == null && !FlashingState.leftState) {
-			FlxTransitionableState.skipNextTransIn = true;
-			FlxTransitionableState.skipNextTransOut = true;
-			MusicBeatState.switchState(new FlashingState());
-		} else {
 			#if desktop
 			if (!DiscordClient.isInitialized)
 			{
@@ -220,7 +215,6 @@ class TitleState extends MusicBeatState
 			{
 				startIntro();
 			});
-		}
 		#end
 	}
 
