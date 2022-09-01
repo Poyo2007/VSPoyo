10f489fbb3f4e1df921a9f7b8a37e3ad44ed0ca7
Poyo2007 <itssng7@gmail.com>
2022-09-01 16:27:15 -0400

    taken this from saw and msirox 💀

--- source/Main.hx
+++ source/Main.hx
@@ -1,31 +1,13 @@
 package;
 
-import flixel.graphics.FlxGraphic;
 import flixel.FlxG;
 import flixel.FlxGame;
-import flixel.FlxState;
-import openfl.Assets;
-import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
-import openfl.events.Event;
-import openfl.display.StageScaleMode;
-import openfl.geom.Rectangle;
 
 class Main extends Sprite
 {
-	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
-	var framerate:Int = 60; // How many frames per second the game should run at.
-	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
-	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 	public static var fpsVar:FPS;
-
-	// You can pretty much ignore everything from here on - your code should go in your states.
-
-	public static function main():Void
-	{
-		Lib.current.addChild(new Main());
-	}
 
 	public function new()
 	{
@@ -33,49 +15,19 @@ class Main extends Sprite
 
 		SUtil.gameCrashCheck();
 
-		if (stage != null)
-		{
-			init();
-		}
-		else
-		{
-			addEventListener(Event.ADDED_TO_STAGE, init);
-		}
-	}
-
-	private function init(?E:Event):Void
-	{
-		if (hasEventListener(Event.ADDED_TO_STAGE))
-		{
-			removeEventListener(Event.ADDED_TO_STAGE, init);
-		}
-
-		setupGame();
-	}
-
-	private function setupGame():Void
-	{
 		SUtil.doTheCheck();
 
-		#if !debug
-		initialState = TitleState;
-		#end
-	
 		ClientPrefs.loadDefaultKeys();
+
 		addChild(new FlxGame(0, 0, TitleState, 1, 60, 60, true, false));
+
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
-		Lib.current.stage.align = "tl";
-		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
-		Lib.current.stage.fullScreenSourceRect = new Rectangle(0, 0, 1280, 720);
-		if(fpsVar != null) {
+		if (fpsVar != null)
 			fpsVar.visible = ClientPrefs.showFPS;
-		}
 
 		#if html5
 		FlxG.autoPause = false;
-		FlxG.mouse.visible = false;
-		#elseif android
 		FlxG.mouse.visible = false;
 		#end
 	}
